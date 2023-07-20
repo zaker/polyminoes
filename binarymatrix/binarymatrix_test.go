6e2bab0e63cbcd6f01889e624aa03a292a8bc923
Sondre Naustdal <sondre.naustdal@sbanken.no>
2023-07-20 16:19:23 +0200

    Introduce a binary matrix as data backend
    Export GetBit
    
    Remove tests for moved functionality
    
    mess with rewriting, checkin befor vacation
    
    remove compiler errors
    
    Add expansion, missing rotation and base rotation
    
    Add rot90
    
    Rot90 for level 3 works
    
    Bug fixes and make generator package
    
    Bugfix value check
    
    Add test for set minimum rotation
    
    Add dev container config

--- binarymatrix/binarymatrix_test.go
+++ binarymatrix/binarymatrix_test.go
@@ -14,9 +14,9 @@ func TestNew(t *testing.T) {
 		args args
 		want *BinaryMatrix[uint64]
 	}{
-		{"1x1", args{1, 1}, &BinaryMatrix[uint64]{1, 1, make([]uint64, ((1*1)/64)+1)}},
-		{"3x3", args{3, 3}, &BinaryMatrix[uint64]{3, 3, make([]uint64, ((3*3)/64)+1)}},
-		{"5x5", args{5, 5}, &BinaryMatrix[uint64]{5, 5, make([]uint64, ((5*5)/64)+1)}},
+		{"1x1", args{1, 1}, &BinaryMatrix[uint64]{1, 1, 64, make([]uint64, ((1*1)/64)+1)}},
+		{"3x3", args{3, 3}, &BinaryMatrix[uint64]{3, 3, 64, make([]uint64, ((3*3)/64)+1)}},
+		{"5x5", args{5, 5}, &BinaryMatrix[uint64]{5, 5, 64, make([]uint64, ((5*5)/64)+1)}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -48,7 +48,7 @@ func TestBinaryMatrix_SetBit(t *testing.T) {
 			if err := tt.bm.SetBit(tt.args.x, tt.args.y, tt.args.b); (err != nil) != tt.wantErr {
 				t.Errorf("BinaryMatrix.SetBit() error = %v, wantErr %v", err, tt.wantErr)
 			}
-			got := tt.bm.getBit(tt.args.x, tt.args.y)
+			got := tt.bm.GetBit(tt.args.x, tt.args.y)
 			if !got {
 				t.Errorf("getBit(%d,%d) == false , want true",
 					tt.args.x,
@@ -79,4 +79,57 @@ func TestBinaryMatrix_Code(t *testing.T) {
 		}
 	})
 
+}
+
+func TestBinaryMatrix_Less(t *testing.T) {
+
+	bm := New[uint8](3, 3)
+	bm.SetBit(0, 0, true)
+
+	bm1 := New[uint8](3, 3)
+	bm1.SetBit(2, 2, true)
+	if !bm.Less(bm1) {
+		t.Errorf("BinaryMatrix.Code() = %v, want %v", false, true)
+	}
+	bm2 := New[uint8](3, 3)
+	bm2.SetBit(0, 2, true)
+	if !bm.Less(bm2) {
+		t.Errorf("BinaryMatrix.Code() = %v, want %v", false, true)
+	}
+
+}
+
+func TestLoad(t *testing.T) {
+
+	gen4 := func() *BinaryMatrix[uint8] {
+		t := New[uint8](4, 4)
+		t.SetBit(0, 0, true)
+		t.SetBit(1, 0, true)
+		t.SetBit(2, 0, true)
+		t.SetBit(1, 1, true)
+		return t
+	}
+
+	type args struct {
+		n uint8
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *BinaryMatrix[uint8]
+	}{
+		// TODO: Add test cases.
+		{"1x1", args{1, New[uint8](1, 1).Code()}, New[uint8](1, 1)},
+		{"2x2", args{2, New[uint8](2, 2).Code()}, New[uint8](2, 2)},
+		{"3x3", args{3, New[uint8](3, 3).Code()}, New[uint8](3, 3)},
+		{"4x4", args{4, gen4().Code()}, gen4()},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Load[uint8](tt.args.n, tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Load() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
--- polymino/polymino_test.go
+++ polymino/polymino_test.go
@@ -1,24 +1,90 @@
 package polymino
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestPolymino_StrideCount(t *testing.T) {
+// func TestPolymino_StrideCount(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		size uint
+// 	}{
+// 		{"7", 3},
+// 		{"1B", 4},
+// 		{"S", 4},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			p := LoadPolymino(tt.size, tt.name)
+
+// 			if p.StrideCount().Cmp(p.StrideCount2()) != 0 {
+// 				t.Errorf("Polymino.StrideCount() = %v, want %v", p.StrideCount().Text(62), p.StrideCount2().Text(62))
+// 			}
+// 		})
+// 	}
+// }
+
+func TestPolymino_Expand(t *testing.T) {
+	// type args struct {
+	// 	expPoly map[string]string
+	// }
+
+	// n2 := "AgID"
+	// p2 := polymino.Load(2, n2)
+	// map3 := map[string]string{}
+	// p2.Expand(map3)
+
+	// for k, ps3 := range map3 {
+	// 	p3 := polymino.Load(3, ps3)
+	// 	fmt.Println("-3-", k, ps3)
+	// 	fmt.Println(p3)
+	// 	fmt.Println("-3-")
+
+	// }
+
+	p3 := Load(3, "AwML")
+	map4 := map[string]string{}
+	p3.Expand(map4)
+
+	for k, ps4 := range map4 {
+		p4 := Load(4, ps4)
+		fmt.Println("-4-", k, ps4)
+		fmt.Println(p4)
+		fmt.Println("-4-")
+
+	}
+	// tests := []struct {
+	// 	name string
+	// 	p    Polymino
+	// 	args args
+	// }{
+	// 	// TODO: Add test cases.
+
+	// }
+	// for _, tt := range tests {
+	// 	t.Run(tt.name, func(t *testing.T) {
+	// 		tt.p.Expand(tt.args.expPoly)
+	// 	})
+	// }
+}
+
+func TestPolymino_SetToMinRotation(t *testing.T) {
+
 	tests := []struct {
 		name string
-		size uint
+		size uint8
+		want string
 	}{
-		{"7", 3},
-		{"1B", 4},
-		{"S", 4},
+		{"BAQRAw", 4, "BAQX"},
+		{"BAQX", 4, "BAQX"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := LoadPolymino(tt.size, tt.name)
-
-			if p.StrideCount().Cmp(p.StrideCount2()) != 0 {
-				t.Errorf("Polymino.StrideCount() = %v, want %v", p.StrideCount().Text(62), p.StrideCount2().Text(62))
+			p := Load(tt.size, tt.name)
+			p.SetToMinRotation()
+			if p.Code() != tt.want {
+				t.Errorf("Polymino.StrideCount() = %v, want %v", p.Code(), tt.want)
 			}
 		})
 	}
